5527a7ec4a4e29da124cca024dc698b83d70225d
Raghava Pamula <rp2zd@virginia.edu>
2019-10-14 23:25:34 -0400

    changed signup

--- signup/signup.php
+++ signup/signup.php
@@ -1,7 +1,7 @@
 <?php
-    $db_connection = pg_connect("host=localhost dbname=dm2oitjf83u73 user=tjzntcroxpnjme password=f26ae870fba99aa4ec86ad28f5b4e2df70de121c81324e818122615da4dcb59a");
+    $db_connection = parse_url(getenv("DATABASE_URL"));
 
-    
+
     $fullname = $_POST['name'];
     $email = $_POST['email'];
     $username = $_POST['username'];
@@ -12,12 +12,12 @@
     $state = $_POST['state'];
     $zip = $_POST['zip'];
 
-    
+
     if(pg_fetch_array(pg_query($db_connection,"select * from Users where username = '$username'"))){
         echo "<script>alert('username taken');window.location.href='index.html'</script>";
     } else{
         $sqlinsert="insert into Users(Full_Name,Email,Username,Password,Address,City,State,Zip) values('{$fullname}','{$email}','{$username}','{$hashpass}','{$address}','{$city}','{$state}','{$zip}')";
- 
+
         if(!(pg_query($sqlinsert))){
             echo "<script>alert('failed');window.location.href='index.html'</script>";
         }else{
